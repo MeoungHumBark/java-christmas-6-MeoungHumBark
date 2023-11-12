246e53cbf51314f9c8df1170f64193698a693a08
MeoungHum <emmabark84@gmail.com>
2023-11-12 15:18:53 +0900

    feat(method):weekDiscount

--- src/main/java/christmas/BenefitAmount.java
+++ src/main/java/christmas/BenefitAmount.java
@@ -1,10 +1,14 @@
 package christmas;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 public class BenefitAmount {
     private static final List<Integer> starDate = Arrays.asList(3, 10, 17, 24, 25, 31);
+    private static final List<Integer> weekend = Arrays.asList(1,2,8,9,15,16,22,23,29,30);
+    private static int weekDiscountAmount = -2023;
     private int discountAmount = -1000;
 
     public int specialDiscount(int date) {
@@ -17,10 +21,10 @@ public class BenefitAmount {
         return discountAmount;
     }
 
-    public int weekDiscount() {
-        return -1;
+    public int weekDiscount(int date, int numberOfMain, int numberOfDessert) {
+        if(weekend.contains(date)) return numberOfMain*weekDiscountAmount;
+        return numberOfDessert*weekDiscountAmount;
     }
-
     public int bonus() {
         return -1;
     }
