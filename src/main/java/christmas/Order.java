e4597e3482ed5a555ecd47671f297aa140337259
MeoungHum <emmabark84@gmail.com>
2023-11-12 23:02:08 +0900

    feat(method):processOrder

--- src/main/java/christmas/Order.java
+++ src/main/java/christmas/Order.java
@@ -5,8 +5,13 @@ import java.util.HashMap;
 public class Order {
     private HashMap<String, Integer> order;
 
-    public Order(HashMap<String, Integer> order) {
-        this.order = new HashMap<>(order);
+    public HashMap<String, Integer> processOrder(Object... args) {
+        for (int i = 0; i < args.length; i += 2) {
+            String key = (String) args[i];
+            Integer value = (Integer) args[i + 1];
+            order.put(key, value);
+        }
+        return order;
     }
 
     public HashMap<String, Integer> getOrder() {
