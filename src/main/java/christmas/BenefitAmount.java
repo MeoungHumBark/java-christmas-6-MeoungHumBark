e8826fd9f46903616c66af70323784664156d3f7
MeoungHum <emmabark84@gmail.com>
2023-11-12 19:25:30 +0900

    feat(class):Menu and TotalAmount

--- src/main/java/christmas/BenefitAmount.java
+++ src/main/java/christmas/BenefitAmount.java
@@ -7,23 +7,25 @@ public class BenefitAmount {
     private static final List<Integer> starDate = Arrays.asList(3, 10, 17, 24, 25, 31);
     private static final List<Integer> weekend = Arrays.asList(1,2,8,9,15,16,22,23,29,30);
     private static int weekDiscountAmount = -2023;
-    private int discountAmount = -1000;
 
-    public int specialDiscount(int date) {
-        if(starDate.contains(date)) return discountAmount;
+    private static int christmasDiscountAmount = -1000;
+    private static int specialDiscountAmount = -1000;
+
+    public static int specialDiscount(int date) {
+        if(starDate.contains(date)) return specialDiscountAmount;
         return 0;
     }
 
-    public int christmasDiscount(int date) {
-        if(date >= 1 && date <= 25)  return discountAmount-=100*(date-1);
+    public static int christmasDiscount(int date) {
+        if(date >= 1 && date <= 25)  return christmasDiscountAmount-=100*(date-1);
         return 0;
     }
 
-    public int weekDiscount(int date, int numberOfMain, int numberOfDessert) {
+    public static int weekDiscount(int date, int numberOfMain, int numberOfDessert) {
         if(weekend.contains(date)) return numberOfMain*weekDiscountAmount;
         return numberOfDessert*weekDiscountAmount;
     }
-    public int bonus(int totalAmount) {
+    public static int bonus(int totalAmount) {
         if(totalAmount>=120000) return -25000;
         return 0;
     }
