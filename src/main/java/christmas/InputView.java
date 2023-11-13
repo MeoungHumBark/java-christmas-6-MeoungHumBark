fa343b59868d72ef4af17ed5dd7dfcdbdf3a2f65
MeoungHum <emmabark84@gmail.com>
2023-11-13 20:01:09 +0900

    refactor(method):processOrder

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -6,13 +6,13 @@ public class InputView {
     public static int printVisitDateInput() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
-        int date = Integer.parseInt(input);
-        return date;
+        int dateInput = Integer.parseInt(input);
+        return dateInput;
     }
 
     public static String printOrderInput() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String order = Console.readLine();
-        return order;
+        String OrderInput = Console.readLine();
+        return OrderInput;
     }
 }
--- src/main/java/christmas/Order.java
+++ src/main/java/christmas/Order.java
@@ -5,10 +5,12 @@ import java.util.HashMap;
 public class Order {
     private HashMap<String, Integer> order;
 
-    public HashMap<String, Integer> processOrder(Object... args) {
-        for (int i = 0; i < args.length; i += 2) {
-            String name = (String) args[i];
-            Integer quantity = (Integer) args[i + 1];
+    public HashMap<String, Integer> processOrder(String OrderInput) {
+        String[] orderItems = OrderInput.split(",");
+        for (String orderItem : orderItems) {
+            String[] itemInfo = orderItem.trim().split("-");
+            String name = itemInfo[0];
+            int quantity = Integer.parseInt(itemInfo[1]);
             order.put(name, quantity);
         }
         return order;
