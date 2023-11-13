f957d24af6ec8a4ba8405434f401c912b24a16b3
MeoungHum <emmabark84@gmail.com>
2023-11-13 16:53:27 +0900

    feat(method):calculateTotalPrice

--- src/main/java/christmas/TotalAmount.java
+++ src/main/java/christmas/TotalAmount.java
@@ -1,4 +1,16 @@
 package christmas;
 
+import java.util.HashMap;
+
 public class TotalAmount {
+    public int calculateTotalPrice(HashMap<String, Integer> order) {
+        int totalPrice = 0;
+
+        for (String name : order.keySet()) {
+            int quantity = order.get(name);
+            int price = Menu.getPriceByName(name);
+            totalPrice += (quantity * price);
+        }
+        return totalPrice;
+    }
 }
