c57567fec26b04da1c08f95fc6efabd9d0eed49b
MeoungHum <emmabark84@gmail.com>
2023-11-14 00:41:18 +0900

    feat(exception):printVisitDateInput

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -5,8 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static int printVisitDateInput() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
-        int dateInput = Integer.parseInt(input);
+        int dateInput = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                String input = Console.readLine();
+                dateInput = Integer.parseInt(input);
+                validInput = true;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+        }
         return dateInput;
     }
 
