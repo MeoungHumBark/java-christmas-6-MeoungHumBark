dd7aa5b6688fd31d5b58bd45d4bf50f0fa30fe32
MeoungHum <emmabark84@gmail.com>
2023-11-14 14:56:39 +0900

    refactor(readability):main

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -8,18 +8,24 @@ public class Application {
         int date = InputView.printVisitDateInput();
         String orderInput = InputView.printOrderInput();
         HashMap<String, Integer> order = Order.processOrder(orderInput);
+
         OutputView.printEvent(date);
         OutputView.printOrderMenu(order);
+
         int totalPrice = TotalAmount.calculateTotalPrice(order);
         OutputView.printTotalPrice(totalPrice);
+
         boolean isBonus = TotalAmount.isBonus(totalPrice);
         OutputView.printBonus(isBonus);
+
         int christmasDiscounts = BenefitAmount.christmasDiscount(date);
         int weekDiscounts = BenefitAmount.weekDiscount(date,TotalAmount.quantityOfMain(order),TotalAmount.quantityOfDessert(order));
         int specialDiscounts = BenefitAmount.specialDiscount(date);
         int bonus = BenefitAmount.bonus(isBonus);
+
         int totalBenefit = BenefitAmount.calculateTotalBenefits(christmasDiscounts,weekDiscounts,specialDiscounts,bonus);
         OutputView.printBenefit(christmasDiscounts,weekDiscounts,specialDiscounts,bonus,totalBenefit);
+
         OutputView.printTotalBenefit(totalBenefit);
         OutputView.printPayment(totalPrice,totalBenefit,isBonus);
         OutputView.printBadge(totalBenefit);
